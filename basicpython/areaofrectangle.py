0a1574bd0504f3b6de6deba331464469188069f2
vishalsinghprogrammer <vishalsinghprogrammer@gmail.com>
2025-10-14 22:11:21 +0530

    switch

---
+++ basicpython/areaofrectangle.py
@@ -0,0 +1,4 @@
+length=float(input('Enter Length:'))
+width=float(input('Enter Width:'))
+area= length*width
+print(area)
---
+++ python/if,elif,else.py
@@ -0,0 +1,7 @@
+x = 10
+if x > 15:
+    print("x is greater than 15")
+elif x == 10:
+    print("x is 10")
+else:
+    print("x is less than 10")
