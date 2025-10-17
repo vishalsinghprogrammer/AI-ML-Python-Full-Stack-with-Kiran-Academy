a8015130418c410410c32bd62a6a9c8fcace857f
vishalsinghprogrammer <vishalsinghprogrammer@gmail.com>
2025-10-17 22:17:13 +0530

    sift files

---
+++ basicpython/pre_defined_fun.py
@@ -0,0 +1,17 @@
+day = input("Enter the day: ")
+
+if day in ["Saturday", "Sunday"]:
+    print("It's the weekend.")
+else:
+    print("It's a weekday.")
+
+x = 10
+if x > 15:
+    print("x is greater than 15")
+elif x == 10:
+    print("x is 10")
+else:
+    print("x is less than 10")
+
+name = input("Enter your name: ")
+print("Name is", name)
